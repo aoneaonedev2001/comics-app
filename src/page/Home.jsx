76c4233abc00a7a75801fdcaec49dd7c6324c16a
AoneDev2001 <aonedev2001@gmail.com>
2024-04-16 18:14:11 +0700

    POC Success

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,52 +1,57 @@
-import React from 'react'
+import {useState} from 'react'
 import { motion } from "framer-motion"
 
 const Home = () => {
+    const [ShowA1, setShowA1] = useState(false);
+    console.log(ShowA1);
   return (
-    <div className=' w-[1200px]  mx-auto border-1 border h-[1795px]'>
+    <div className=' max-w-[1200px]    mx-auto border-1 border '>
 
     {/* page 1*/}
       <div
-          className="max-w-full  h-full flex flex-col border-1 border  "
+          className="bg-contain bg-no-repeat h-[1800px] max-w-[full]   flex flex-col border-1 border  "
           style={{
-          backgroundImage: `url(/full.png)`,
-          backgroundSize: "cover"
+          backgroundImage: `url(/full.png)`
            }}
        >
-            <div className='flex flex-row justify-between items-center   relative mx-[96px] top-[405px]'>
+            <div className='flex flex-row justify-between items-center h-[20%]   relative mx-[96px] top-[405px]'>
                 
-                <motion.div className='w-[35%]  h-[400px]'
-                    style={{
-                        backgroundImage: `url(/mask-group-2.png)`,
-                        backgroundSize: "cover",
-                    }}> 
-                    
-            {/* Test text */}
-                    <div className='mt-[50px] bg-red-200 border-[3px]  border-black h-[50px]'>
-                        <h1>ffffffffffffffffffffffffff</h1>
-                    </div>
-                    
-                </motion.div>
-                <motion.div  className='w-[30%]  h-[400px] border-[3px] border-black'
-                    style={{
-                        backgroundImage: `url(/mask-group-3.png)`,
-                        backgroundSize: "cover",
-                    }}>
-            {/* Test text */}
-                        <div className='absolute mt-[50px] bg-red-200 border-[3px]  border-black h-[50px]'>
-                            <h1>ffffffffffffffffffffffffffggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg</h1>
-                        </div> 
-                    
-                </motion.div>
-                <motion.div className='w-[30%]  h-[400px] border-[3px] border-black'
-                    style={{
-                        backgroundImage: `url(/mask-group-4.png)`,
-                        backgroundSize: "cover",
-                    }}> 
-                    {/* Test text */}
-                    
-                    
-                </motion.div>
+            <motion.div
+        onClick={() => setShowA1(!ShowA1)}
+        className='w-[30%] h-full border-[1px] border-black'
+        style={{
+          backgroundImage: `url(/mask-group-2.png)`,
+          backgroundSize: 'cover',
+        }}
+      ></motion.div>
+      <motion.div
+        className='w-[30%] h-full border-[1px] border-black'
+        variants={{
+          hidden: { opacity: 0, x: -75 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial='hidden'
+        animate={ShowA1 ? 'visible' : 'hidden'}
+        transition={{ duration: 0.5, delay: ShowA1 ? 0.5 : 0 }}
+        style={{
+          backgroundImage: `url(/mask-group-3.png)`,
+          backgroundSize: 'cover',
+        }}
+      ></motion.div>
+      <motion.div
+        className='w-[30%] h-full border-[1px] border-black'
+        variants={{
+          hidden: { opacity: 0, x: -75 },
+          visible: { opacity: 1, x: 0 },
+        }}
+        initial='hidden'
+        animate={ShowA1 ? 'visible' : 'hidden'}
+        transition={{ duration: 0.5, delay: ShowA1 ? 0.75 : 0 }}
+        style={{
+          backgroundImage: `url(/mask-group-4.png)`,
+          backgroundSize: 'cover',
+        }}
+      ></motion.div>
             </div>
         </div>
 
--- src/page/Test.jsx
+++ src/page/Test.jsx
@@ -1,42 +1,113 @@
-import React from 'react';
+import {useState,useRef,useEffect}from 'react';
+import { motion,useInView ,useScroll,useTransform,useAnimation} from "framer-motion"
 
 const Test = () => {
+   
+  const [ShowA1,setShowA1]=useState(false)
+   //console.log(ShowA1);
+
   return (
-    <div className='max-w-[1200px]  mx-auto border-[3px] mt-[30px] '>
+    <div  className='max-w-[1200px]  mx-auto border-[3px] mt-[30px] '>
+  {/*1 Page */}
       <div className='box-1 relative'>
+    {/*2 Backgroud-Img */}
         <img className='box-items-1 max-w-full ' src="full.png" alt="" />
+    {/*3 Box-Content */}
         <div className='absolute inset-0 '>
-    {/* box 1 page */}
-          <div className='max-h-[100%] h-[1800px]  '>
-       
-       {/* box-items-1 */}
+          <div className='max-h-[100%] h-[1800px]'>
+       {/*4 Box-items-1 */}
              <div className='flex flex-row justify-between items-center relative top-[22%]  mx-[8%]  h-[22%] '>
-                <div className='w-[30%]  h-full border-[1px] border-black'
+                <motion.div className='w-[30%]  h-full border-[1px] border-black'
+
+                        initial={{opacity:0,x:-75}}          
+                        whileInView={{opacity:1,x:0}}              
+                        transition={{duration: 0.5,delay: 0.25}}   
                         style={{
                             backgroundImage: `url(/mask-group-2.png)`,
                             backgroundSize: "cover",
                         }}> 
-                </div>
-                <div className='w-[30%]  h-full border-[1px] border-black'
+                </motion.div>
+                <motion.div className='w-[30%]  h-full border-[1px] border-black'
+                        initial={{opacity:0,x:-75}}          
+                        whileInView={{opacity:1,x:0}}              
+                        transition={{duration: 0.5,delay: 0.50}}
                         style={{
                             backgroundImage: `url(/mask-group-3.png)`,
                             backgroundSize: "cover",
                         }}> 
-                </div>
-                <div className='w-[30%]  h-full border-[1px] border-black'
+                </motion.div>
+                <motion.div className='w-[30%]  h-full border-[1px] border-black'
+                        initial={{opacity:0,x:-75}}          
+                        whileInView={{opacity:1,x:0}}              
+                        transition={{duration: 0.5,delay: 0.75}}
                         style={{
                             backgroundImage: `url(/mask-group-4.png)`,
                             backgroundSize: "cover",
                         }}> 
-                </div>
+                </motion.div>
             </div>
           </div>
-
         </div>
-        
-        
-        
       </div>
+
+      {/*2 Page */}
+      <div className='box-1 relative'>
+    {/*2 Backgroud-Img */}
+        <img className='box-items-1 max-w-full ' src="full.png" alt="" />
+    {/*3 Box-Content */}
+        <div className='absolute inset-0 '>
+          <div className='max-h-[100%] h-[1800px]'>
+       {/*4 Box-items-1 */}
+       
+             <div  className='flex flex-row justify-between items-center relative top-[22%]  mx-[8%]  h-[22%] '>
+                  <motion.div 
+                    onClick={()=>setShowA1(!ShowA1)}  
+                    className='w-[30%]  h-full border-[1px] border-black'   
+                    style={{
+                        backgroundImage: `url(/mask-group-2.png)`,
+                        backgroundSize: "cover",
+                    }}> 
+                  </motion.div>   
+                  <motion.div
+                    className='w-[30%] h-full border-[1px] border-black'
+                    variants={{
+                      hidden: { opacity: 0, x: -20 },
+                      visible: { opacity: 1, x: 0 },
+                    }}
+                    initial='hidden'
+                    animate={ShowA1 ? 'visible' : 'hidden'}
+                    transition={{ duration: 0.5, delay: ShowA1 ? 0.5 : 0 }}
+                    style={{
+                      backgroundImage: `url(/mask-group-3.png)`,
+                      backgroundSize: 'cover',
+                    }}>
+                    </motion.div>
+                    <motion.div
+                      className='w-[30%] h-full border-[1px] border-black'
+                      variants={{
+                        hidden: { opacity: 0, x: -75 },
+                        visible: { opacity: 1, x: 0 },
+                      }}
+                      initial='hidden'
+                      animate={ ShowA1 ? { opacity: 1, x: 0 } : { opacity: 0, x: -75 }}
+                      transition={{ duration: 0.5, delay: ShowA1 ? 0.75 : 0 }}
+                      style={{
+                        backgroundImage: `url(/mask-group-4.png)`,
+                        backgroundSize: 'cover',
+                      }}>
+                        <div className='bg-red-300 h-full '>
+                          <h2 className='bg-red text-[2vw]'>Aone5555555555555555</h2>
+                        </div>
+                        
+                      </motion.div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <img src="full2.jpg" alt="" />
+
+      
     </div>
   );
 };
