b098893f9c4a39d79c1c8e4cd1592960633cb0a5
AoneDev2001 <aonedev2001@gmail.com>
2024-04-20 13:01:23 +0700

    Deploy

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Alret18 from '../component/Alret18'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div  className=' max-w-[1200px] flex justify-center h-screen items-center   mx-auto border-1 border '>
       <Alret18/>
+      
+      <Link to="/predater1" className=''>Ep1</Link>
+      <Link to="/predater2" className=''>Ep2</Link>
     </div>
   )
 }
